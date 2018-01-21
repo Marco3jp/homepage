fbc4fe418f05c98efa39c984c57564515b796347
Marco3jp <k_kimu_jr@yahoo.co.jp>
2018-01-21 11:19:21 +0900

    change algorithm. Do not disturb rendering.

--- js/movable.js
+++ js/movable.js
@@ -1,18 +1,17 @@
 window.onload = function(){
-    var htmlText="";
-    var words=[["作りたいものを","作れる人に"],["なんとかなるよ","絶対だいじょうぶだよ"],["律子だけを","追いかけ続ける"]];
-    var words_ref=["0","http://ccsakura-official.com","http://idolmaster.jp/event/765as_newyearlive.php"]
-    var words_position=["28.6vh","14.6vh","21.6vh"];
-    var displayWordNum=Math.floor(Math.random()*words.length);
+    var WORDS_NUM = 3;
+    var displayWordNum=Math.floor(Math.random()*WORDS_NUM);
 
-    if(words_ref[displayWordNum]==="0"){
-        htmlText = "<h1> <span>" + words[displayWordNum][0] + "</span> <br /> <span> " + words[displayWordNum][1] + " </span> </a> </h1>";
-    }else{
-        htmlText = "<h1> <a href=\"" + words_ref[displayWordNum] + "\"> <span>" + words[displayWordNum][0] + "</span> <br /> <span> " + words[displayWordNum][1] + " </span> </a> </h1>";
-    }
-
-    document.getElementsByClassName("word")[0].innerHTML = htmlText;
-    if(document.documentElement.clientHeight<document.documentElement.clientWidth){
-        document.getElementsByClassName("word")[0].style.marginLeft= words_position[displayWordNum];
+    switch (displayWordNum) {
+        case 0:
+            document.getElementById("word01").style.display = "block";
+            break;
+        case 1:
+            document.getElementById("word02").style.display = "block";
+            break;
+        case 2:
+            document.getElementById("word03").style.display = "block";
+            break;
+        default:
     }
 }
